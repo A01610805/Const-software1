46b92c740a50536deddc7e2dd1fad24910d9c3cb
Jose <A01610805@tec.mx>
2022-02-22 19:58:16 -0600

    Lab 5

---
+++ Lab_5/Lab5.js
@@ -0,0 +1,6 @@
+function descripcion(){
+    let div_imagen = document.getElementById("imagen");  //Convertimos la imagen en una variable
+    let ruta_imagen = "https://beerbank.mx/1407-large_default/hercules-super-lupe.jpg";
+    let alt_imagen = "Imagen de una cerveza Super Lupe";
+    div_imagen.innerHTML = "Material design es un framework responsivo muy amigable que nos ayuda a diseñar páginas web con mucha más facilidad que si lo hiciéramos de cero. Consiste en una serie de funciones predefinidas escritas en CSS y JS, que podemos personalizar y utilizar dentro de nuestro código.";  //Le otorgamos valores a la imagen
+}
