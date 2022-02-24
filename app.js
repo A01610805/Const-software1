9fe40bb0c0be49f3db0e7571135581bfd36363e0
Jose <A01610805@tec.mx>
2022-02-24 12:42:09 -0600

    sort

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ console.log("hola desde node");
 const filesystem = require('fs');
 
 filesystem.writeFileSync('hola.txt', 'Hola desde node');        //Escribimos 'hola desde node' dentro del archivo 'hola.txt'
+
+const arreglo = [5000, 60, 90, 100, 10, 20, 0, 120, 2000, 340, 1000, 50];
+
+for (let item of arreglo) {     //Se crea automáticamente una fila priorizada por tiempos, lo que ordena el arreglo.
+    setTimeout(() => {
+        console.log(item);
+    }, item);
+}
