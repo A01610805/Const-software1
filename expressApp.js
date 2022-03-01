aa463a24682be90041ea1f15d237a101f56ea4d4
Jose <A01610805@tec.mx>
2022-03-01 12:29:27 -0600

    middlewares

---
+++ expressApp.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+//Middleware: un software que va enmedio
+app.use((request, response, next) => {
+    console.log("Middleware!");
+    next(); //Le permite a la petición avanzar hacia el siguiente middleware
+});
+
+app.use('/instrumentos', (request, response, next) => {
+    response.send('Respuesta de la ruta "/instrumentos"');
+});
+
+app.use((request, response, next) => {
+    console.log('Otro Middleware!');
+    response.send('Hola mundo!'); //Manda la respuesta y da por terminada la ejecución
+});
+
+
+
+app.listen(3000);
+
