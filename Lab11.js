fd7f19f9d89501c08cb96f2dcbbdbbf42f1330c2
Jose <A01610805@tec.mx>
2022-03-02 20:23:53 -0600

    Lab11 Rename

--- Lab11.js
+++ Lab11.js
@@ -2,20 +2,24 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser'); 
 
-//Middleware: un software que va enmedio
+const misRutas = require('./routes/instrumentos.routes');
+
+app.use('/modulo', misRutas);
+
+/* //Middleware: un software que va enmedio
 app.use((request, response, next) => {
     console.log("Middleware!");
     next(); //Le permite a la petición avanzar hacia el siguiente middleware
 });
 
-app.use('/instrumentos', (request, response, next) => {
+app.use('/instrumentos', (request, response, next) => {     //agrega una ruta
     response.send('Respuesta de la ruta "/instrumentos"');
 });
 
 app.use((request, response, next) => {
     console.log('Otro Middleware!');
     response.send('Hola mundo!'); //Manda la respuesta y da por terminada la ejecución
-});
+}); */
 
 
 
--- routes/instrumentos.routes.js
+++ routes/instrumentos.routes.js
@@ -2,3 +2,9 @@ const express = require('express');     //Archivo para administrar diferentes ru
 
 const router = express.Router();
 
+router.get('/ruta', (request, response, next) => {
+    response.send('Respuesta de la ruta "/modulo/ruta"'); 
+});
+
+module.exports = router;
+                        
