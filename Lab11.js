fc46ab71b4883c09678a737ee3df3b8b6254568f
Jose <A01610805@tec.mx>
2022-03-07 17:54:26 -0600

    routes

--- Lab11.js
+++ Lab11.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser'); 
+app.use(bodyParser.urlencoded({extended: false}));
 
 const misRutas = require('./routes/instrumentos.routes');
 
-app.use('/modulo', misRutas);
+app.use('/', misRutas);
 
-/* //Middleware: un software que va enmedio
+// app.use(express.static(path.join(__dirname, 'public')));    // damos acceso directo a la carpeta "public". Lab12
+
+// Middleware: un software que va enmedio
 app.use((request, response, next) => {
     console.log("Middleware!");
     next(); //Le permite a la petición avanzar hacia el siguiente middleware
 });
 
+/*
 app.use('/instrumentos', (request, response, next) => {     //agrega una ruta
     response.send('Respuesta de la ruta "/instrumentos"');
 });
@@ -19,7 +23,7 @@ app.use('/instrumentos', (request, response, next) => {     //agrega una ruta
 app.use((request, response, next) => {
     console.log('Otro Middleware!');
     response.send('Hola mundo!'); //Manda la respuesta y da por terminada la ejecución
-}); */
+}); */               
 
 
 
--- routes/instrumentos.routes.js
+++ routes/instrumentos.routes.js
@@ -2,9 +2,33 @@ const express = require('express');     //Archivo para administrar diferentes ru
 
 const router = express.Router();
 
-router.get('/ruta', (request, response, next) => {
-    response.send('Respuesta de la ruta "/modulo/ruta"'); 
+const instrumentos = ["Guitarra", "Piano", "Saxofón"];
+
+
+
+router.get('/nuevo', (request, response, next) => {
+    let respuesta = ('<!DOCTYPE html><html lang="es-mx"><head><meta charset="utf-8"><title>Nuevo Instrumento</title></head><body><h1 id="guitarras">Este sitio es para ingresar nuevos instrumentos</h1><form action="nuevo" method="POST" <label for=nombre">Nombre del instrumento: </label><input type="text" id="nombre" name="nombre" placeholder="Violín"><br><br><input type="submit" value="Enviar"></form><br><br><a href="/">Regresar a la lista de instrumentos</a></body>');
+    response.send(respuesta); 
 });
 
+router.post('/nuevo', (request, response, next) => {
+    console.log(request.body);
+    instrumentos.push(request.body.nombre);
+    response.redirect('/');
+});
+
+router.use('/', (request, response, next) => {
+    let respuesta = ('<!DOCTYPE html><html lang="es-mx"><head><meta charset="utf-8"><title>Instrumentos</title></head><body><h1 id="principal">Este sitio es de instrumentos musicales</h1><ul>');
+    for (let i in instrumentos) {
+    respuesta += '<li>' + instrumentos[i] + '</li>';
+    }
+    respuesta += '</ul><br><br><a href="nuevo">Agregar un instrumento</a><br><br></body></html>';
+    response.send(respuesta);
+});
+
+
+/*response.setHeader('Content-Type', 'text/html');      // Lab 12
+        '<!DOCTYPE html><html lang="es-mx"><head><meta charset="utf-8"><title>Nuevo Instrumento</title></head><body><h1 id="guitarras">Este sitio es para ingresar nuevos instrumentos</h1><form action="nuevo" method="POST" <input type="text" id="nombre" name="nombre" placeholder="violín"><br><br><input type="submit" value="Enviar"></form><br><br><a href="/">Regresar a la lista de instrumentos</a></body>'
+*/
 module.exports = router;
                         
