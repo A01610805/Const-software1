c3f56fa624872c4f2f3b23391f82a3f741229f87
Jose <A01610805@tec.mx>
2022-03-07 18:02:17 -0600

    404

--- routes/instrumentos.routes.js
+++ routes/instrumentos.routes.js
@@ -2,7 +2,7 @@ const express = require('express');     //Archivo para administrar diferentes ru
 
 const router = express.Router();
 
-const instrumentos = ["Guitarra", "Piano", "Saxofón"];
+const instrumentos_array = ["Guitarra", "Piano", "Saxofón"];
 
 
 
@@ -13,22 +13,26 @@ router.get('/nuevo', (request, response, next) => {
 
 router.post('/nuevo', (request, response, next) => {
     console.log(request.body);
-    instrumentos.push(request.body.nombre);
+    instrumentos_array.push(request.body.nombre);
     response.redirect('/');
 });
 
-router.use('/', (request, response, next) => {
+router.get('/', (request, response, next) => {
     let respuesta = ('<!DOCTYPE html><html lang="es-mx"><head><meta charset="utf-8"><title>Instrumentos</title></head><body><h1 id="principal">Este sitio es de instrumentos musicales</h1><ul>');
-    for (let i in instrumentos) {
-    respuesta += '<li>' + instrumentos[i] + '</li>';
+    for (let i in instrumentos_array) {
+    respuesta += '<li>' + instrumentos_array[i] + '</li>';
     }
     respuesta += '</ul><br><br><a href="nuevo">Agregar un instrumento</a><br><br></body></html>';
     response.send(respuesta);
 });
 
+router.use('/', (request, response, next) => {
+    response.statusCode = 404;
+    let respuesta = ('<!DOCTYPE html><html><head><title>Instrumentos | Not Found</title></head><body><h1 id="404">Este sitio no existe, amigo.</h1></body>');
+    response.send(respuesta);
+})
 
-/*response.setHeader('Content-Type', 'text/html');      // Lab 12
-        '<!DOCTYPE html><html lang="es-mx"><head><meta charset="utf-8"><title>Nuevo Instrumento</title></head><body><h1 id="guitarras">Este sitio es para ingresar nuevos instrumentos</h1><form action="nuevo" method="POST" <input type="text" id="nombre" name="nombre" placeholder="violín"><br><br><input type="submit" value="Enviar"></form><br><br><a href="/">Regresar a la lista de instrumentos</a></body>'
-*/
+
+
 module.exports = router;
                         
