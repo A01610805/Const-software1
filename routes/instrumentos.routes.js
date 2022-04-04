61813ac5b08a2a114360e5aa9187f6c67f255400
Jose <A01610805@tec.mx>
2022-04-03 21:28:39 -0500

    Lab 18 terminado

--- routes/instrumentos.routes.js
+++ routes/instrumentos.routes.js
@@ -4,9 +4,9 @@ const isAuth = require('../util/is-auth.js');
 
 const instrumentosController = require('../controllers/instrumentos_controller');
 
-router.get('/instrumentos', isAuth, instrumentosController.listaInst);
-router.get('/nuevo', isAuth, instrumentosController.get_nuevo);
-router.post('/nuevo', isAuth, instrumentosController.post_nuevo);
+router.get('/instrumentos', instrumentosController.listaInst);
+router.get('/nuevo', instrumentosController.get_nuevo);
+router.post('/nuevo', instrumentosController.post_nuevo);
 router.get('/', isAuth, instrumentosController.inicio);
 router.get('/:ID_instrumento', isAuth, instrumentosController.filtrar);   // Para filtrar los instrumentos
 router.use('/', isAuth, instrumentosController.error);
